d57b41645e849fd0708cb318699d1573e3573a9b
erben <erben@umich.edu>
2023-11-22 22:07:56 +0300

    update

--- onepint/src/pages/index.js
+++ onepint/src/pages/index.js
@@ -4,73 +4,35 @@ import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-import { motion } from "framer-motion"
 import Link from 'next/link';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 
 const inter = Inter({ subsets: ['latin'] });
+gsap.registerPlugin(ScrollTrigger);
+
 
 export default function Home() {
-  // useEffect(() => {
-  //   // Wrap GSAP code in useEffect to ensure it runs after components are mounted
-  //   const containers = gsap.utils.toArray(".container");
-  //   containers.forEach((container) => {
-  //     let tl = gsap.timeline({
-  //       scrollTrigger: {
-  //         trigger: container,
-  //         pin: true,
-  //         pinSpacing: false,
-  //         scrub: true,
-  //         markers: true
-  //       }
-  //     });
-    
-  //     tl.to(container, {
-  //       autoAlpha: 1,
-  //       visibility: 'visible'
-  //     }).to(
-  //       container,
-  //       {
-  //         autoAlpha: 0,
-  //         visibility: 'hidden'
-  //       },
-  //       0.5
-  //     );
-    
-  //     // Log messages for debugging
-  //     tl.eventCallback("onStart", () => console.log("Animation started"));
-  //     tl.eventCallback("onComplete", () => console.log("Animation completed"));
-  //   });
-    
-  //   // Make sure ScrollTrigger is available
-  //   if (ScrollTrigger) {
-  //     gsap.registerPlugin(ScrollTrigger);
-      
-  //     containers.forEach((container) => {
-  //       let tl = gsap.timeline({
-  //         scrollTrigger: {
-  //           trigger: container,
-  //           pin: true,
-  //           pinSpacing: false,
-  //           scrub: true,
-  //           markers: true
-  //         }
-  //       });
+  const [ref, inView] = useInView({
+    triggerOnce: false, // This will ensure the animation triggers again when you scroll back up
+  });
+
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start('visible');
+    }
+  }, [animation, inView]);
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   
-  //       tl.to(container, {
-  //         autoAlpha: 1,
-  //         visibility: 'visible' // Ensure visibility is set to 'visible'
-  //       }).to(
-  //         container,
-  //         {
-  //           autoAlpha: 0,
-  //           visibility: 'hidden' // Set visibility to 'hidden' when fading out
-  //         },
-  //         0.5
-  //       );
-  //     });
-  //   }
-  // }, []); // Empty dependency array ensures this runs once after component mount
+
   
   return (
     <>
@@ -80,10 +42,47 @@ export default function Home() {
 
       <main className={`${styles.main} ${inter.className}`}>
         <div className={`${styles.title}`}>
-          <div className={`${styles.container}`}>
+        <motion.div
+           ref={ref}
+           variants={containerVariants}
+           initial="hidden"
+           whileInView="visible"
+           animate={inView ? 'visible' : 'hidden'}
+           className={`${styles.container}`}
+          >
             Meet someone cool
-          </div>
-          <div className={`${styles.container}`}>
+          </motion.div>
+          <motion.div
+             ref={ref}
+              variants={containerVariants}
+              initial="hidden"
+              whileInView="visible"
+              animate={inView ? 'visible' : 'hidden'}
+              className={`${styles.container}`}
+          >
+            Over a pint
+          </motion.div>
+          <motion.div
+             ref={ref}
+              variants={containerVariants}
+              initial="hidden"
+              whileInView="visible"
+              animate={inView ? 'visible' : 'hidden'}
+              className={`${styles.container}`}
+          >
+            Every week
+          </motion.div>
+          <motion.div
+            ref={ref}
+            variants={containerVariants}
+            initial="hidden"
+            whileInView="visible"
+            animate={inView ? 'visible' : 'hidden'}
+            className={`${styles.container}`}
+          >
+            <Link href={''}>Apply now</Link>
+          </motion.div>
+          {/* <div className={`${styles.container}`}>
             Over a pint
           </div>
           <div className={`${styles.container}`}>
@@ -94,7 +93,7 @@ export default function Home() {
           </div>
           <div className={`${styles.container_button}`}>
             <Link href={''}>Apply now</Link>
-          </div>
+          </div> */}
         </div>
         <div className={`${styles.animated_gradient}`}></div>
       </main>
