dfc8275beddaba3dcda2785f5078b47d1bccefd7
erben <erben@umich.edu>
2023-11-25 14:08:00 +0300

    "": ""

--- onepint/src/components/form.js
+++ onepint/src/components/form.js
@@ -1,25 +1,15 @@
-import React from 'react';
+require('dotenv').config();
+import React, { useState } from 'react';
 import styles from '@/styles/Home.module.css';
-import { text, field } from 'react';
-import RequiredTextField from '../components/formFields';
 import { createClient } from '@supabase/supabase-js';
-import { useState } from 'react';
+import RequiredTextField from '../components/formFields';
 
+const supabaseUrl = 'https://fuxtlegfhnhfmqmcprrg.supabase.co.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1eHRsZWdmaG5oZm1xbWNwcnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5MDI1MjMsImV4cCI6MjAxNjQ3ODUyM30.Xn3IiHNfj2pUoqdxKNj9rUcT5P0-pMsR885YJ7OvRQo';
+const supabaseKey = process.env.SUPABASE_KEY;
+console.log(process.env.SUPABASE_KEY);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-
-
-
-
-
-const Form = ({}) => {
-
+const Form = () => {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [company, setCompany] = useState('');
@@ -30,42 +20,55 @@ const Form = ({}) => {
 
         // Check if email is already used
         const { data, error } = await supabase
-        .from('responses')
-        .select()
-        .eq('email', email);
+            .from('responses')
+            .select()
+            .eq('email', email);
 
         if (error) {
-        console.error('Error checking email:', error.message);
-        return;
+            console.error('Error checking email:', error.message);
+            return;
         }
 
         if (data && data.length > 0) {
-        setError('Email is already used.');
-        return;
+            setError('Email is already used.');
+            return;
         }
 
         // If email is not used, save the response
-        const { error: saveError } = await supabase.from('responses').insert([{ email, name, company }]);
+        const { error: saveError } = await supabase
+            .from('responses')
+            .insert([{ email, name, company }]);
 
         if (saveError) {
-        console.error('Error saving response:', saveError.message);
+            console.error('Error saving response:', saveError.message);
         } else {
-        console.log('Response saved successfully!');
-        // Perform any additional actions after successful submission
+            console.log('Response saved successfully!');
+            // Perform any additional actions after successful submission
         }
     };
-    
-        return (
-            <>
+
+    return (
+        <>
             <div onSubmit={handleSubmit}>
-                <RequiredTextField placeholder="Full name" value="name"  onChange={(e) => setName(e.target.value)}/>
-                <RequiredTextField placeholder="Company/School" value="company" onChange={(e) => setAnswer(e.target.value)}/>
-                <RequiredTextField placeholder="Email" value="email" onChange={(e) => setEmail(e.target.value)}/>
+                <RequiredTextField
+                    placeholder="Full name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <RequiredTextField
+                    placeholder="Company/School"
+                    value={company}
+                    onChange={(e) => setCompany(e.target.value)}
+                />
+                <RequiredTextField
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
             <button>Hello</button>
-            </>
-        );
-    };
-
+        </>
+    );
+};
 
 export default Form;
